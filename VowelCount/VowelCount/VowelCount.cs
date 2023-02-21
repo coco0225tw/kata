53d08998ec4d90c6b07dbbfab51e4bf01a44129b
River <coco0958231520@gmail.com>
2023-02-21 16:07:26 +0800

    [VowelCount]Create new solution

--- VowelCount/VowelCount/VowelCount.cs
+++ VowelCount/VowelCount/VowelCount.cs
@@ -1,24 +1,6 @@
 ﻿namespace VowelCount;
 
-public static class Kata
+public class VowelCount
 {
-    public static int GetVowelCount(string str)
-    {
-        var vowelCount = 0;
-
-        int index;
-        for (index = 0; index < str.Length; index++)
-        {
-            var n = str[index];
-            if (
-                n == 'a' |
-                n == 'e' |
-                n == 'i' |
-                n == 'o' |
-                n == 'u'
-            ) vowelCount++;
-        }
-
-        return vowelCount;
-    }
+    
 }
