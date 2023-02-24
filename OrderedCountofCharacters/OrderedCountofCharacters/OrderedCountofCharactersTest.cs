63144c360da3dcb0475ae32aaf06fae695d1b334
River <coco0958231520@gmail.com>
2023-02-24 11:50:11 +0800

    [OrderedCountofCharacters]Update

--- OrderedCountofCharacters/OrderedCountofCharacters/OrderedCountofCharactersTest.cs
+++ OrderedCountofCharacters/OrderedCountofCharacters/OrderedCountofCharactersTest.cs
@@ -7,11 +7,65 @@ public class Tests
     {
         Assert.That(
             OrderedCountofCharacters.OrderedCount("abracadabra"), Is.EqualTo(new List<Tuple<char, int>>() {
-                new Tuple('a', 5),
-                tuple('b', 2),
-                tuple('r', 2),
-                tuple('c', 1),
-                tuple('d', 1)
+                Tuple.Create('a', 5),
+                Tuple.Create('b', 2),
+                Tuple.Create('r', 2),
+                Tuple.Create('c', 1),
+                Tuple.Create('d', 1)
+            }));
+    }
+    [Test]
+    public void TestCase2()
+    {
+        Assert.That(
+            OrderedCountofCharacters.OrderedCount("everything"), Is.EqualTo(new List<Tuple<char, int>>() {
+                Tuple.Create('e', 2),
+                Tuple.Create('v', 1),
+                Tuple.Create('r', 1),
+                Tuple.Create('y', 1),
+                Tuple.Create('t', 1),
+                Tuple.Create('h', 1),
+                Tuple.Create('i', 1),
+                Tuple.Create('n', 1),
+                Tuple.Create('g', 1)
+
+            }));
+    }
+    [Test]
+    public void TestCase3()
+    {
+        Assert.That(
+            OrderedCountofCharacters.OrderedCount("capybara"), Is.EqualTo(new List<Tuple<char, int>>() {
+                Tuple.Create('c', 1),
+                Tuple.Create('a', 3),
+                Tuple.Create('p', 1),
+                Tuple.Create('y', 1),
+                Tuple.Create('b', 1),
+                Tuple.Create('r', 1)
+            }));
+    }
+    [Test]
+    public void TestCase4()
+    {
+        Assert.That(
+            OrderedCountofCharacters.OrderedCount("macarena"), Is.EqualTo(new List<Tuple<char, int>>() {
+                Tuple.Create('m', 1),
+                Tuple.Create('a', 3),
+                Tuple.Create('c', 1),
+                Tuple.Create('r', 1),
+                Tuple.Create('e', 1),
+                Tuple.Create('n', 1)
+            }));
+    }
+    [Test]
+    public void TestCase5()
+    {
+        Assert.That(
+            OrderedCountofCharacters.OrderedCount("ymcaymca"), Is.EqualTo(new List<Tuple<char, int>>() {
+                Tuple.Create('y', 2),
+                Tuple.Create('m', 2),
+                Tuple.Create('c', 2),
+                Tuple.Create('a', 2)
             }));
     }
 }
