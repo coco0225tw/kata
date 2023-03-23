7de1f7b587203d1b94a4eabbc4dd173d55318fbd
River <coco0958231520@gmail.com>
2023-03-23 10:35:29 +0800

    Update DismantleVowelTrolls.cs

--- DismantleVowelTrolls/DismantleVowelTrolls/DismantleVowelTrolls.cs
+++ DismantleVowelTrolls/DismantleVowelTrolls/DismantleVowelTrolls.cs
@@ -4,10 +4,7 @@ public class DismantleVowelTrolls
 {
     public static string Disemvowel(string str)
     {
-        string data = "aeiouAEIOU";
-        List<char> charsToRemove = new List<char>();
-        charsToRemove.AddRange(data);
-      
+        var charsToRemove = new List<char>() {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I','O','U'};
         charsToRemove.ForEach(c => str = str.Replace(c.ToString(), String.Empty));
      
         return str;
