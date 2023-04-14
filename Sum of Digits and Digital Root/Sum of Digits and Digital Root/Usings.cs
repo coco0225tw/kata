8da97205b9a7c1a7db0b489ccb2d402b0e6775e8
River <coco0958231520@gmail.com>
2023-04-14 11:04:02 +0800

    [Sum of Digits / Digital Root]convert to iteration

--- Sum of Digits and Digital Root/Sum of Digits and Digital Root/Usings.cs
+++ Sum of Digits and Digital Root/Sum of Digits and Digital Root/Usings.cs
@@ -5,14 +5,18 @@ public class Number
 {
     public static int DigitalRoot(long n)
     {
-        if(n < 10)
-            return (int)n; //cast
-    
-        var sum = 0;
-        while (n != 0) {
-            sum = (int)(sum + n % 10); //除以10得到的餘數 //cast
-            n /= 10; //除以10得到的整數
+        while (true)
+        {
+            if (n < 10) return (int)n; //cast
+
+            var sum = 0;
+            while (n != 0)
+            {
+                sum = (int)(sum + n % 10); //除以10得到的餘數 //cast
+                n /= 10; //除以10得到的整數
+            }
+
+            n = sum;
         }
-        return DigitalRoot(sum);
     }
 }
