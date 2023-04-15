53f67c46f6bdd5b4d2c14564bc8598845a33743f
River <coco0958231520@gmail.com>
2023-04-15 08:33:15 +0800

    [Who likes it]extract method

--- Who likes it/Who likes it/Usings.cs
+++ Who likes it/Who likes it/Usings.cs
@@ -3,6 +3,14 @@ public static class Kata
 {
     public static string Likes(string[] name)
     {
+        var s = HandleSingularOrPlural(name);
+        
+        
+        return s;
+    }
+    
+    private static string HandleSingularOrPlural(string[] name)
+    {
         var singularOrPlural = name.Length <= 1 ? "" : "s";
         var str = $"like{singularOrPlural} it.";
         return str;
