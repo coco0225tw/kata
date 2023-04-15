74dfbd640d3e421c9f2a43e42549bc34aa900c25
River <coco0958231520@gmail.com>
2023-04-15 08:57:41 +0800

    [Who likes it] add testcase2

--- Who likes it/Who likes it/UnitTest1.cs
+++ Who likes it/Who likes it/UnitTest1.cs
@@ -9,4 +9,9 @@ public class Tests
         Assert.That(Kata.Likes(Array.Empty<string>()), Is.EqualTo("no one likes this"));
 
     }
+    [Test]
+    public void when_input_array_contains_one_person()
+    {
+        Assert.That(Kata.Likes(new string[] {"Peter"}), Is.EqualTo("Peter likes this"));
+    }
 }
