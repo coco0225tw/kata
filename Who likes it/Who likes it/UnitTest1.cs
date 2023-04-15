33f66ea18aaa7fde684872bbe6a5d84ea6e2dd40
River <coco0958231520@gmail.com>
2023-04-15 09:05:10 +0800

    [Who likes it] add testcase to test more than four person

--- Who likes it/Who likes it/UnitTest1.cs
+++ Who likes it/Who likes it/UnitTest1.cs
@@ -26,4 +26,16 @@ public class Tests
     {
         Assert.That(Kata.Likes(new string[] {"Max", "John", "Mark"}), Is.EqualTo("Max, John and Mark like this"));
     }
+
+    [Test]
+    public void when_input_array_contains_four_person()
+    {
+        Assert.That(Kata.Likes(new string[] {"Alex", "Jacob", "Mark", "Max"}), Is.EqualTo("Alex, Jacob and 2 others like this"));
+    }
+    
+    [Test]
+    public void when_input_array_contains_six_person()
+    {
+        Assert.That(Kata.Likes(new string[] {"Alex", "Jacob", "Mark", "Max", "Ray", "Eric"}), Is.EqualTo("Alex, Jacob and 4 others like this"));
+    }
 }
