a0cfcebc9114ff323ca0fe8fd7ef4bcb4222c6f7
River <coco0958231520@gmail.com>
2023-04-15 09:25:02 +0800

    [WhoLikesThis]remove type reference

--- Who likes it/Who likes it/WhoLikesThisUnitTests.cs
+++ Who likes it/Who likes it/WhoLikesThisUnitTests.cs
@@ -14,36 +14,36 @@ public class Tests
     [Test]
     public void when_input_array_contains_one_person()
     {
-        Assert.That(Kata.Likes(new string[] {"Peter"}), Is.EqualTo("Peter likes this"));
+        Assert.That(Kata.Likes(new[] {"Peter"}), Is.EqualTo("Peter likes this"));
     }
     
     [Test]
     public void when_input_array_contains_two_person()
     {
-        Assert.That(Kata.Likes(new string[] {"Jacob", "Alex"}), Is.EqualTo("Jacob and Alex like this"));
+        Assert.That(Kata.Likes(new[] {"Jacob", "Alex"}), Is.EqualTo("Jacob and Alex like this"));
     }
     
     [Test]
     public void when_input_array_contains_three_person()
     {
-        Assert.That(Kata.Likes(new string[] {"Max", "John", "Mark"}), Is.EqualTo("Max, John and Mark like this"));
+        Assert.That(Kata.Likes(new[] {"Max", "John", "Mark"}), Is.EqualTo("Max, John and Mark like this"));
     }
 
     [Test]
     public void when_input_array_contains_four_person()
     {
-        Assert.That(Kata.Likes(new string[] {"Alex", "Jacob", "Mark", "Max"}), Is.EqualTo("Alex, Jacob and 2 others like this"));
+        Assert.That(Kata.Likes(new[] {"Alex", "Jacob", "Mark", "Max"}), Is.EqualTo("Alex, Jacob and 2 others like this"));
     }
     
     [Test]
     public void when_input_array_contains_six_person()
     {
-        Assert.That(Kata.Likes(new string[] {"Alex", "Jacob", "Mark", "Max", "Ray", "Eric"}), Is.EqualTo("Alex, Jacob and 4 others like this"));
+        Assert.That(Kata.Likes(new[] {"Alex", "Jacob", "Mark", "Max", "Ray", "Eric"}), Is.EqualTo("Alex, Jacob and 4 others like this"));
     }
     
     [Test]
     public void when_input_array_contains_invalid_string()
     {
-        Assert.That(Kata.Likes(new string[] {"Alex", "Jacob", "Mark", " ", "  ", "", "Jason"}), Is.EqualTo("Alex, Jacob and 2 others like this"));
+        Assert.That(Kata.Likes(new[] {"Alex", "Jacob", "Mark", " ", "  ", "", "Jason"}), Is.EqualTo("Alex, Jacob and 2 others like this"));
     }
 }
