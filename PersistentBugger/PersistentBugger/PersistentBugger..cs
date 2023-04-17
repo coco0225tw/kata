79e24111b5470ff066a98579ebfc9c41c1194b7f
River <coco0958231520@gmail.com>
2023-04-17 09:42:59 +0800

    add files and modified

--- PersistentBugger/PersistentBugger/PersistentBugger..cs
+++ PersistentBugger/PersistentBugger/PersistentBugger..cs
@@ -1,32 +1,28 @@
 namespace PersistentBugger;
 
-public class Persist 
+public abstract class Persist 
 {
     public static int Persistence(long n)
     {
-        while (true)
-        {
-            // your code
-            var times = 0;
+        var times = 0;
             long multiply = 1;
     
             if(n/10==0 || n < 0) return 0;
-    
-            while(true)
+
+            while (true)
             {
-                while(n!=0)
+                while (n != 0)
                 {
-                    multiply*=n%10;
-                    n/=10;
-        
+                    multiply *= n % 10;
+                    n /= 10;
+
                 }
+
                 times++;
-      
-                if(multiply/10==0) return times;
+
+                if (multiply / 10 == 0) return times;
                 n = multiply;
                 multiply = 1;
             }
-        }
-
     }
 }
