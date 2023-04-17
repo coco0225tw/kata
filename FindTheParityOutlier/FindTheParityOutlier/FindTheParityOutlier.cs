5b681a8f7e8f5b6ab63dc3beea3a6f3000806410
River <coco0958231520@gmail.com>
2023-04-17 09:54:16 +0800

    [FindTheParityOutlier] update test and method

--- FindTheParityOutlier/FindTheParityOutlier/FindTheParityOutlier.cs
+++ FindTheParityOutlier/FindTheParityOutlier/FindTheParityOutlier.cs
@@ -4,8 +4,8 @@ public class Kata
 {
     public static int Find(int[] integers)
     {
-        var evenNum = integers.Where(integer => integer % 2 == 0).ToArray();
-        var oddNum = integers.Where(integer => integer % 2 == 1);
+        var evenNum = integers.Where(integer => integer % 2 == 0 && integer >= 0).ToArray();
+        var oddNum = integers.Where(integer => integer % 2 == 1 && integer >= 0);
         return evenNum.Length == 1 ? evenNum.First() : oddNum.First();
     }
 }
--- FindTheParityOutlier/FindTheParityOutlier/FindTheParityOutlierUnitTests.cs
+++ FindTheParityOutlier/FindTheParityOutlier/FindTheParityOutlierUnitTests.cs
@@ -11,4 +11,10 @@ public class Tests
         int[] input = {2,6,8,-10,3}; 
         Assert.That(Kata.Find(input), Is.EqualTo(3));
     }
+    [Test]
+    public void return_even_int()
+    {
+        int[] input = {5,1,8,-10,3}; 
+        Assert.That(Kata.Find(input), Is.EqualTo(8));
+    }
 }
